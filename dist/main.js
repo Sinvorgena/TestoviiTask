01c0b6e051125d02bf6afac0a146c5c58ad2717d
unknown <pismecho@gmail.com>
2021-03-22 16:56:28 +0500

    Сделал все таски по js

--- dist/main.js
+++ dist/main.js
@@ -103,14 +103,72 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-
 let burgerBtn = document.querySelector(".Header__burgerMenu")
 let burgerMenu = document.querySelector(".burgerMenu")
 let body = document.querySelector("body")
+let ModuleBtn = document.querySelector(".ContactUs__btn")
+let ModuleBox = document.querySelector(".moduleBox")
+let NameInput = document.querySelector("#NameInput")
+let emailInput = document.querySelector("#emailInput")
+let MessageTextArea = document.querySelector("#MessageTextArea")
+let SubmitLetsTalkBtn = document.querySelector("#SubmitLetsTalkBtn")
+let FormRequiredError = document.querySelector(".FormRequiredError")
+let Form = document.querySelector(".moduleBox__form")
+let sucsessFormSendMessage = document.querySelector(".sucsessFormSend")
 
+SubmitLetsTalkBtn.addEventListener("mouseenter",()=>{
+    FormRequiredError.classList.add("active")
+    console.log("fvsgfdgdf");
+    setTimeout(()=>{
+        FormRequiredError.classList.remove("active")
+    },3000)
+})
+SubmitLetsTalkBtn.disabled = true
 
-burgerBtn.addEventListener("click", ()=>{
-    if(burgerMenu.classList.contains("active")){
+SubmitLetsTalkBtn.addEventListener("click", ()=>{
+
+})
+Form.addEventListener("submit", ()=>{
+    console.log("FDFS");
+    sucsessFormSendMessage.classList.add("active")
+    ModuleBtn.classList.remove("active")
+    ModuleBox.classList.remove("active")
+    body.classList.remove("ModuleActive")
+})
+
+NameInput.addEventListener("input", ()=>{
+    if(NameInput.value && emailInput.value && MessageTextArea.value){
+        SubmitLetsTalkBtn.disabled = false
+        FormRequiredError.classList.remove("active")
+        FormRequiredError.innerHTML = ""
+    } else{
+        SubmitLetsTalkBtn.disabled = true
+        FormRequiredError.classList.add("active")
+
+    }
+})
+emailInput.addEventListener("input", ()=>{
+    if(NameInput.value && emailInput.value && MessageTextArea.value){
+        SubmitLetsTalkBtn.disabled = false
+        FormRequiredError.classList.remove("active")
+        FormRequiredError.innerHTML = ""
+    } else{
+        SubmitLetsTalkBtn.disabled = true
+        FormRequiredError.classList.add("active")
+    }
+})
+MessageTextArea.addEventListener("input", ()=>{
+    if(NameInput.value && emailInput.value && MessageTextArea.value){
+        SubmitLetsTalkBtn.disabled = false
+        FormRequiredError.classList.remove("active")
+        FormRequiredError.innerHTML = ""
+    } else{
+        SubmitLetsTalkBtn.disabled = true
+        FormRequiredError.classList.add("active")
+    }
+})
+burgerBtn.addEventListener("click", () => {
+    if (burgerMenu.classList.contains("active")) {
         burgerMenu.classList.remove("active")
         burgerBtn.classList.remove("active")
         body.classList.remove("BurgerActive")
@@ -120,6 +178,42 @@ burgerBtn.addEventListener("click", ()=>{
         body.classList.add("BurgerActive")
     }
 })
+
+ModuleBtn.addEventListener("click", () => {
+    if (ModuleBtn.classList.contains("active")) {
+        ModuleBtn.classList.remove("active")
+        ModuleBox.classList.remove("active")
+        body.classList.remove("ModuleActive")
+    } else {
+        ModuleBtn.classList.add("active")
+        ModuleBox.classList.add("active")
+        body.classList.add("ModuleActive")
+    }
+})
+
+
+window.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("ContactUs__btn")) {
+        if (ModuleBtn.classList.contains("active")) {
+            if (!e.target.classList.contains("moduleField")) {
+                ModuleBtn.classList.remove("active")
+                ModuleBox.classList.remove("active")
+                body.classList.remove("ModuleActive")
+            }
+        }
+    }
+})
+
+window.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("sucsessFormSend")) {
+        if (sucsessFormSendMessage.classList.contains("active")) {
+            if (!e.target.classList.contains("moduleField")) {
+                sucsessFormSendMessage.classList.remove("active")
+            }
+        }
+    }
+})
+
 
 
 
